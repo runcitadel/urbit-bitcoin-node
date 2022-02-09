9ae37a2945bd488480649ba23b5f81e4705b007f
Aaron Dewes <aaron.dewes@protonmail.com>
2022-02-09 17:16:07 +0100

    Remove unused env vars

--- rpc/mainnet-start.sh
+++ rpc/mainnet-start.sh
@@ -4,14 +4,14 @@
 #sudo /usr/bin/append-to-hosts "$(ip -4 route list match 0/0 | awk '{print $3 "\thost.docker.internal"}')"
 echo Running modified mainnet script...
 # docker-compose.yml passes the folllowing env vars:
-# $BITCOIN_RPC_AUTH, $BITCOIN_PORT, $BITCOIN_IP, $BITCOIN_PORT,
+# $$BITCOIN_RPC_USER, $BITCOIN_RPC_PASS, $BITCOIN_PORT, $BITCOIN_IP, $BITCOIN_PORT,
 # $ELECTRUM_IP, and $ELECTRUM_PORT
 export PROXY_PORT=55555
 export BITCOIN_RPC_PORT=$BITCOIN_RPC_PORT
 export BITCOIN_IP=$BITCOIN_IP
 export ELECTRUM_IP=$ELECTRUM_IP
 export ELECTRUM_PORT=$ELECTRUM_PORT
-export BITCOIN_RPC_AUTH=$BITCOIN_RPC_AUTH
+export BITCOIN_RPC_USER=$BITCOIN_RPC_USER
 export BITCOIN_RPC_PASS=$BITCOIN_RPC_PASS
 export INTERNAL_IP=$INTERNAL_IP
 
